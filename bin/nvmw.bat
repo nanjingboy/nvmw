7daf2027f3b851a49223f4e45e99f8e394f76e8d
Tom <hzlhu.dargon@gmail.com>
2013-10-29 16:16:00 +0800

    fixed local variable error

--- bin/nvmw.bat
+++ bin/nvmw.bat
@@ -1,19 +1,19 @@
 @echo off
 
+setlocal
 for /f "tokens=2*" %%a in ('reg query HKEY_CURRENT_USER\Software\Node.js /v InstallPath') do set "node_path=%%b\node.exe"
-
 if  exist "%node_path%" (
   "%node_path%" "%~dp0\..\lib\cli.js" %1 %2
+  endlocal
+
   if "%1" == "use" (
     call :set_enviroment %1
   ) else if "%1" == "deactivate" (
     call :set_enviroment %1
   )
   exit /b %ERRORLEVEL%
-) else (
-  echo The system version of Node not found
-  exit /b 1
 )
+exit /b 1
 
 :set_enviroment
   if %ERRORLEVEL% == 0 (
